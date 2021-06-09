7de0aa2006eeef6a0af8f7f202ea6baf0fcfa6e2
Mikhail Koryukov <mezhovkin@yandex.ru>
2021-06-09 13:18:40 +0500

    feat: добавить управляемые компоненты

--- src/components/EditProfilePopup.js
+++ src/components/EditProfilePopup.js
@@ -1,6 +1,22 @@
+import React from 'react'
+import { CurrentUserContext } from '../contexts/CurrentUserContext'
+
 import PopupWithForm from './PopupWithForm'
 
 function EditProfilePopup(props) {
+  const currentUser = React.useContext(CurrentUserContext)
+
+  const [name, setName] = React.useState('')
+  const [description, setDescription] = React.useState('')
+
+  function handleChangeName(e) {
+    setName(e.target.value);
+  }
+
+  function handleChangeDescription(e) {
+    setDescription(e.target.value);
+  }
+
   return (
     <PopupWithForm
       title='Редактировать профиль'
@@ -9,10 +25,10 @@ function EditProfilePopup(props) {
       isOpen={props.isOpen}
       onClose={props.onClose}
     >
-      <input className="popup__input popup__input_type_name" id="name" name="name" type="text" placeholder="Имя"
+      <input className="popup__input popup__input_type_name" value={name} onChange={handleChangeName} id="name" name="name" type="text" placeholder="Имя"
         minLength="2" maxLength="40" required />
       <span className="popup__input-error" id="name-error"></span>
-      <input className="popup__input popup__input_type_vocation" id="vocation" name="vocation" type="text"
+      <input className="popup__input popup__input_type_vocation" value={description} onChange={handleChangeDescription} id="vocation" name="vocation" type="text"
         placeholder="Деятельность" minLength="2" maxLength="200" required />
       <span className="popup__input-error" id="vocation-error"></span>
     </PopupWithForm>
